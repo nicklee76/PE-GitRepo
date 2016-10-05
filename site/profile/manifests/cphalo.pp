f173fa9bfa58a0fa8daa922f419a8d3726174db7
nicklee76 <coolnicklee@hotmail.com>
2016-10-05 15:11:26 -0700

    changed the parameter agent_key to agentkey

--- site/profile/manifests/cphalo.pp
+++ site/profile/manifests/cphalo.pp
@@ -1,6 +1,6 @@
 class profile::cphalo {
   class { 'cloudpassage':
-    agent_key => lookup('agent_key'),
-    tags      => $::server_role,
+    agentkey => lookup('agent_key'),
+    tags     => $::server_role,
   }
 }
