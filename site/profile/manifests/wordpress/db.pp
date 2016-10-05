fca32e3075439cb81b1c1b3ce1f42135b532dc8d
nicklee76 <coolnicklee@hotmail.com>
2016-10-05 11:18:24 -0700

    added mysql classes

--- site/profile/manifests/wordpress/db.pp
+++ site/profile/manifests/wordpress/db.pp
@@ -1,2 +1,18 @@
 class profile::wordpress::db {
+
+  class { '::mysql::server':
+    root_password           => lookup('wordpress_passwd'),
+    remove_default_accounts => true,
+    override_options        => {
+      mysqld => {'bind-address' => '0.0.0.0'},
+    },
+  }
+  
+  mysql::db { 'wordpress':
+    user     => lookup('wordpress_username'),
+    password => lookup('wordpress_passwd'),
+    host     => 'localhost',
+    grant    => ['ALL'],
+  }
+
 }
