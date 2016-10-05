13a6f814fc61a4bb6822a53ca71b154f8da35668
nicklee76 <coolnicklee@hotmail.com>
2016-10-05 16:22:24 -0700

    changed dot to dash

--- site/profile/facts.d/server_role.sh
+++ site/profile/facts.d/server_role.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-VAR=$(/opt/puppetlabs/bin/puppet config print certname --section agent | awk -F . '{print $1}')
+VAR=$(/opt/puppetlabs/bin/puppet config print certname --section agent | awk -F - '{print $1}')
 echo server_role=$VAR
