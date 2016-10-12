cadaa596bbcb5cd2bf1902619151e9a4791ca768
nicklee76 <coolnicklee@hotmail.com>
2016-10-12 16:41:45 -0700

    added server_label

--- site/profile/manifests/cphalo.pp
+++ site/profile/manifests/cphalo.pp
@@ -1,6 +1,7 @@
 class profile::cphalo {
   class { 'cloudpassage':
-    agentkey => lookup('agent_key'),
-    tags     => $::server_role,
+    agentkey     => lookup('agent_key'),
+    tags         => $::server_role,
+    server_label => $::server_label,
   }
 }
