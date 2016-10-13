09385801edaf707b15cc2da63f7461f6abf6b372
nicklee76 <coolnicklee@hotmail.com>
2016-10-12 17:09:37 -0700

    Update cphalo.pp

--- site/profile/manifests/cphalo.pp
+++ site/profile/manifests/cphalo.pp
@@ -2,6 +2,6 @@ class profile::cphalo {
   class { 'cloudpassage':
     agentkey     => lookup('agent_key'),
     tags         => $::server_role,
-    server_label => $::server_label,
+    serverlabel => $::serverlabel,
   }
 }
